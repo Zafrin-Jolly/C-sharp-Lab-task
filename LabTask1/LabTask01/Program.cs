290350d4a5a4dfe6fe7b7d79fd4bda7e492d6490
Zafrin-Jolly <72722584+Zafrin-Jolly@users.noreply.github.com>
2020-10-19 11:13:09 +0600

    lab 1 complete

---
+++ LabTask1/LabTask01/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabTask01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* 1. Console.WriteLine("Hello");
+            Console.WriteLine("Zafrin Sultana"); */
+
+            /* 2. double a = 10;
+            double b = 20.2;
+            double sum = a + b;
+            Console.WriteLine("The sum is "+sum);*/
+
+            /*3. double a = 100;
+            double b = 23;
+            double result= a / b;
+            Console.WriteLine("The result of the division "+result); */
+
+            /*4. double a = (8 * 6) - 5;
+            double b = (55 + 9) % 9;
+            double c = 20 + -3 * 5 / 8;
+            double d = 5 + 15 / 3 * 2 - 8 % 3;
+            Console.WriteLine("The result of a " + a);
+            Console.WriteLine("The result of b " + b);
+            Console.WriteLine("The result of c " + c);
+            Console.WriteLine("The result of d " + d); */
+
+            /*5. double a = 100;
+            double b = 10;
+            Console.WriteLine("The result of the sum " + (a + b));
+            Console.WriteLine("The result of the multiply " + (a * b));
+            Console.WriteLine("The result of the subtraction " + (a - b));
+            Console.WriteLine("The result of the division " + (a / b));
+            Console.WriteLine("The result of the remainder " + (a % b)); */
+
+            /*6. double a = ((25.5 * 3.5 - 3.5 * 3.5) / (40.5 - 4.5));
+            Console.WriteLine("The result of the division " + a); */
+
+            /*7. double width = 5.5; double height = 8.5;
+            Console.WriteLine("The Area is " + width + "*" + height +"="+(width*height));
+            Console.WriteLine("The perimeter is "+ 2 + "*" +"(" +width + "+"+height+") =" +(2*(width+height))); */
+
+        }
+    }
+}
---
+++ Labtask02/Labtask02/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Labtask02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* int i;
+             Console.WriteLine("Enter the value of n");
+             int n = int.Parse(Console.ReadLine());
+             Console.WriteLine("The even values from 1 to "+ n + "are");
+             for (i = 2; i <= n; i += 2)
+             {
+                 Console.WriteLine(i);
+             } */
+            for (int i = 0; i <4; i += 1)
+            {
+                for (int j = 0; j < 6; j += 1)
+                {
+                    if (i % 2 == 0)
+                        Console.Write("X");
+                    else
+                        Console.Write("0");
+
+                }
+                else
+                {
+                    if( j=)
+                }
+                
+
+
+            /*3. for(int i = 1; i <=5; i += 1)
+            {
+                for (int j = i; j<i+5; j+= 1)
+                {
+                    Console.Write(j);
+                }
+                Console.WriteLine();
+
+            } */
+
+                    /*4. int  i, oddS = 0, evenS = 0;
+                    Console.WriteLine("Enter the value of n");
+                    int n = int.Parse(Console.ReadLine());
+                    for(i=1;1<=n;i++)
+                    {
+                        if(i%2 ==0)
+
+                            evenS = evenS + i;
+                else
+                                oddS = oddS + i;
+                        }
+                    Console.WriteLine("Sum of even numbers = " + evenS);
+                     Console.WriteLine("Sum of odd numbers = " + oddS);
+                    */
+
+
+
+        }
+    }
+}
