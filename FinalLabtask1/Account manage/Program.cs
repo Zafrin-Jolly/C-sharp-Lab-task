934260bdc79bad5771ecd3cf4a92e1da9136a502
Zafrin-Jolly <72722584+Zafrin-Jolly@users.noreply.github.com>
2020-11-23 11:19:12 +0600

    final lab task -1

---
+++ FinalLabtask1/Account manage/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Account_manage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Account a1 = new Saving("Zafrin", "12344", 5000);
+            a1.ShowInfo();
+            a1.Deposit(23238);
+            a1.Withdraw(12190);
+            a1.ShowInfo();
+            Console.WriteLine();
+            /* int a = Int32.Parse(DateTime.Now.Year.ToString());
+             Console.WriteLine(DateTime.Now.Year.ToString());*/
+            Account a2 = new Fixed("Zafrin", "12344", 5420, 4, 2016);
+            a2.ShowInfo();
+            a2.Deposit(3000);
+            a2.Withdraw(2320);
+            a2.ShowInfo();
+
+        }
+    }
+}
---
+++ FinalLabtask1/Account manage/Saving.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Account_manage
+{
+   
+    
+        class Saving : Account
+        {
+            private int limitOfMonthlyTransaction;
+
+            public int LimitOfMonthlyTransaction
+            {
+                get { return limitOfMonthlyTransaction; }
+                set { limitOfMonthlyTransaction = value; }
+            }
+            public Saving()
+            {
+                Console.WriteLine("Empty savings Constructor.");
+            }
+            public Saving(String accName, string accId, double balance) : base(accName, accId, balance)
+            {
+                this.limitOfMonthlyTransaction = 5;
+            }
+
+            public override void Withdraw(int amount)
+            {
+                if (amount <= (Balance - 500))
+                {
+                    Balance -= amount;
+                }
+                else
+                    Console.WriteLine("Withdraw failed.");
+            }
+
+            public override void ShowInfo()
+            {
+                Console.WriteLine("Account Holder Name : " + AccountHolderName);
+                Console.WriteLine("Account ID : " + AccId);
+                Console.WriteLine("Balance : " + Balance);
+
+            }
+        }
+    
+}
